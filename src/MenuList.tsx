89deaab821831f63c2c394679ae395ff723aa2d8
Abhimanyu Gupta <abhimanyu.gupta@flairsoftgroup.com>
2024-05-15 02:04:59 +0530

    first commit

---
+++ src/MenuList.tsx
@@ -0,0 +1,35 @@
+const menuLists = [
+  {
+    categoryName: "South Indian",
+    menu: [
+      { name: "Idli Sambhar", price: "40" },
+      { name: "Sambhar Wada", price: "50" },
+      { name: "Idli Fry", price: "50" },
+      { name: "Plain Dosa", price: "70" },
+      { name: "Masala Dosa", price: "90" },
+      { name: "Mysore Masala Dosa", price: "100" },
+      { name: "Paneer Dosa", price: "100" },
+      { name: "Plain Uttapam", price: "60" },
+      { name: "Onion Uttapam", price: "70" },
+      { name: "Mix Uttapam", price: "80" },
+    ],
+  },
+  {
+    categoryName: "Chinese",
+    menu: [
+      { name: "Veg Noodles", price: "99" },
+      { name: "Veg Hakka Noodles", price: "110" },
+      { name: "Veg Shezwan Noodles", price: "120" },
+      { name: "manchurian Noodles", price: "120" },
+      { name: "Fried Rice", price: "99" },
+      { name: "Shezwan Rice", price: "120" },
+      { name: "Paneer Rice", price: "130" },
+      { name: "Crispy Veg", price: "140" },
+      { name: "Onion Chilli Potato", price: "130" },
+      { name: "Veg Soup", price: "60" },
+      { name: "Hot & Sour Soup", price: "60" },
+      { name: "Tomato Soup", price: "60" },
+    ],
+  },
+];
+export default menuLists;
